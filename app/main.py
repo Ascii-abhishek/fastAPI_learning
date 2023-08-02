377f65a2e81bdd862bb6055216618eabe9de33e1
Abhishek Pathak <abhishek56987@gmail.com>
2023-08-02 20:40:31 +0530

    using sqlalchemy

--- app/main.py
+++ app/main.py
@@ -1,84 +1,116 @@
-from typing import Annotated
+from typing import Annotated, Optional
 from pydantic import BaseModel
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Response, status, HTTPException, Depends
+from fastapi.responses import JSONResponse
 import uvicorn
+from random import randrange
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
+from sqlalchemy.orm import Session
+import models
+from database import engine, get_db
 
-
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
-
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hey World"}
+# this is used for data validation
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True  # using default value
+    # extra: str | None = "optional"  -> Optional field, user may or may not send
+    # extra_2: Optional[int] = None  # also optional
+    # ann_ext = Annotated[str | None]
 
 
-@app.put("/items/{item_id}")
-async def create_item(item_id: int, item: Item, q: str | None = None):
-    result = {"item_id": item_id, **item.model_dump()}
-    if q:
-        result.update({"q": q})
-    if item.tax:
-        price_with_tax = item.price + item.tax
-        result.update({"price_with_tax": price_with_tax})
-    return result
+try:
+    conn = psycopg2.connect(
+        host="localhost",
+        database="fastapi",
+        user="postgres",
+        password="56987",
+        cursor_factory=RealDictCursor,
+    )
+    cursor = conn.cursor()
+    print("Databse connection successful")
+except Exception as error:
+    print("connectiopn failed: ", error)
+    time.sleep(2)
 
 
-# @app.get("/models/{model_name}")
-# async def get_model(model_name: ModelName):
-#     if model_name is ModelName.alexnet:
-#         return {"model_name": model_name, "message": "Deep Learning FTW!"}
-
-#     if model_name.value == "lenet":
-#         return {"model_name": model_name, "message": "LeCNN all the images"}
-
-#     return {"model_name": model_name, "message": "Have some residuals"}
+@app.get("/allposts")
+def get_posts(db: Session = Depends(get_db)):
+    # cursor.execute("""SELECT * from posts""")
+    # my_posts = cursor.fetchall()
+    my_posts = db.query(models.Post).all()
+    return {"data": my_posts}
 
 
-@app.get("/users/{user_id}/items/{item_id}")
-async def read_user_item(
-    user_id: int,
-    item_id: str,
-    short: bool = False,
-    q: Annotated[
-        str | None,
-        Query(
-            alias="item-query",
-            title="Query string",
-            description="Query string for the items to search in the database that have a good match",
-            min_length=3,
-            max_length=50,
-            pattern="^fixedquery$",
-            deprecated=False,
-        ),
-    ] = None,
-):
-    """
-    it has both path_parameters and query_parameters
-    http://127.0.0.1:8000/users/121/items/item_id/?q=fixedquery
-
-
-    :param int user_id: required
-    :param str item_id: required
-    :param bool short: default, will be False if not passed
-    :param str q: optional, None by default, you can opt to not pass this parameter
-    """
-
-    item = {"item_id": item_id, "owner_id": user_id}
-    if q:
-        item.update({"q": q})
-    if not short:
-        item.update(
-            {"description": "This is an amazing item that has a long description"}
+@app.get("/posts/{id}")
+def get_posts(id: int, db: Session = Depends(get_db)):
+    # cursor.execute("""  SELECT * from posts where id = %s""", str(id))
+    # my_post = cursor.fetchone()
+    my_post = db.query(models.Post).filter(models.Post.id == id).first()
+    if not my_post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Post with id: {id} not found",
         )
-    return item
+    return {"data": my_post}
 
 
-if __name__ == "__main__":
-    uvicorn.run(app=app)
+@app.post("/posts")
+def create_posts(post: Post, db: Session = Depends(get_db)):
+    # cursor.execute(
+    #     """INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """,
+    #     (post.title, post.content, post.published),
+    # )
+    # my_post = cursor.fetchone()
+    # conn.commit()
+    new_post = models.Post(**post.model_dump())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+    return {"data": new_post}
+    # return JSONResponse(
+    #     content={"data": my_post}, status_code=status.HTTP_201_CREATED
+    # )
+
+
+@app.delete("/posts/{id}")
+def delete_post(id: int, db: Session = Depends(get_db)):
+    # cursor.execute("""  DELETE from posts where id = %s RETURNING *""", str(id))
+    # deleted_post = cursor.fetchone()
+    # conn.commit()
+    deleted_posts = db.query(models.Post).filter(models.Post.id == id)
+    if not deleted_posts.first():
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Post with id: {id} does not exist",
+        )
+    deleted_posts.delete(synchronize_session=False)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post, db: Session = Depends(get_db)):
+    # cursor.execute(
+    #     """UPDATE posts set title = %s, content = %s, published = %s where id = %s RETURNING * """,
+    #     (post.title, post.content, post.published, str(id)),
+    # )
+    # updated_post = cursor.fetchone()
+    # conn.commit()
+    updated_posts = db.query(models.Post).filter(models.Post.id == id)
+    if updated_posts.first() == None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Post with id: {id} does not exist",
+        )
+    
+    updated_posts.update(post.model_dump(), synchronize_session=False)
+    db.commit()
+    return {"data": updated_posts.first()}
